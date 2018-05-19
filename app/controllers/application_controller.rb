7fc289dbeee28a283bc3d85cac608250245d293d
arfullight <tmimvhuang@gmail.com>
2018-05-18 21:58:05 -0700

    implement authenticate function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
+
+  private
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
