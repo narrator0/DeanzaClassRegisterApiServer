60f32581b83164737814bd18e561475c8c87319c
arfullight <tmimvhuang@gmail.com>
2018-05-28 13:31:59 -0700

    point ip to new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '45.56.82.238', user: 'deploy', roles: %w{app db web}
+server '45.33.111.177', user: 'deploy', roles: %w{app db web}
 
 # rvm settings
 set :rvm_custom_path, '/usr/share/rvm'
