8dd65ef104a55574a23e42f5ea05cd6f7483ba52
arfullight <tmimvhuang@gmail.com>
2018-07-05 05:13:24 +0800

    use rescue to prevent retrys if there is too many retries, my portal will block our ip

--- app/workers/update_course_data_worker.rb
+++ app/workers/update_course_data_worker.rb
@@ -2,27 +2,31 @@ class UpdateCourseDataWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    require_relative '../../lib/scraper/myportal_scraper'
-    termcode = Rails.application.credentials.termcode
-    quarter  = Rails.application.credentials.quarter
-    course_data = DeAnzaScraper::MyportalScraper.new.scrape(termcode)
+    begin
+      require_relative '../../lib/scraper/myportal_scraper'
+      termcode = Rails.application.credentials.termcode
+      quarter  = Rails.application.credentials.quarter
+      course_data = DeAnzaScraper::MyportalScraper.new.scrape(termcode)
 
-    Course.transaction do
-      course_data.each do |data|
-        if course = Course.find_by(crn: data[:crn], quarter: quarter)
-          course.attributes = data
+      Course.transaction do
+        course_data.each do |data|
+          if course = Course.find_by(crn: data[:crn], quarter: quarter)
+            course.attributes = data
 
-          if course.changed.include?('status')
-            course.subscribers.each do |user|
-              UserMailer.notify_status_change(user, course, course.status_was, course.status).deliver_later!
+            if course.changed.include?('status')
+              course.subscribers.each do |user|
+                UserMailer.notify_status_change(user, course, course.status_was, course.status).deliver_later!
+              end
             end
-          end
 
-          course.save if course.changed?
-        else
-          Course.create(data)
+            course.save if course.changed?
+          else
+            Course.create(data)
+          end
         end
       end
     end
+  rescue
+    # prvent any exception. do not retry
   end
 end
