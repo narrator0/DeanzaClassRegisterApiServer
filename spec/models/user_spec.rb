5ba6c6c8903e53e48ae3604fd74c16d9f258b6f4
arfullight <tmimvhuang@gmail.com>
2018-10-24 09:19:12 -0700

    create notification model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe User, type: :model do
   it { should have_many(:subscribe_courses).through(:notify_subscriptions).source(:course) }
   it { should have_many(:like_courses).through(:likes).source(:course) }
   it { should have_many(:calendar_courses).through(:calendars).source(:course) }
+  it { should have_many(:notifications) }
 end
