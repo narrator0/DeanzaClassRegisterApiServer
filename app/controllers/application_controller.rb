a925d71c1ea0e5f524c6ad4bb166525dfdcd950b
arfullight <tmimvhuang@gmail.com>
2018-10-26 23:31:25 -0700

    add sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
+  before_action :set_raven_context
 
   private
   def authorize_request
     @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+
+  def set_raven_context
+    Raven.user_context(id: @current_user.try(:id)) # or anything else in session
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
