f160bef02e7d9a68958c76a4194ecfab8f8dee4b
arfullight <tmimvhuang@gmail.com>
2019-02-01 14:13:34 -0800

    add user.name to Users

--- app/controllers/application_with_view_controller.rb
+++ app/controllers/application_with_view_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationWithViewController < ActionController::Base
   skip_before_action :verify_authenticity_token
   before_action :set_raven_context
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def authorize_request
@@ -10,6 +11,10 @@ class ApplicationWithViewController < ActionController::Base
   def set_raven_context
     Raven.user_context(id: @current_user.try(:id)) # or anything else in session
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_203237) do
+ActiveRecord::Schema.define(version: 2019_02_01_215651) do
 
   create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "crn"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_203237) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
